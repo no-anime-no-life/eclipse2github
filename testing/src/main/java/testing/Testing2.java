2a985476f661bab8c6313e182e7bc758a27224bc
karthik <karthikthnr@gmail.com>
2018-09-02 18:46:31 -0500

    First Push

---
+++ testing/src/main/java/testing/Testing2.java
@@ -0,0 +1,19 @@
+package testing;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class Testing2 {
+
+	public static void main( String[] args ) throws FileNotFoundException
+    {
+    	final String a ="Hello World";
+    	System.setOut(new PrintStream(a));
+        System.out.println( "Hello World!" );
+    }
+    
+    public static String calculate()
+    {
+    	return "Hello World";
+    }
+}
---
+++ testing/src/main/resources/AppTest.java
@@ -0,0 +1,34 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	assertEquals("Hello World",testingPpt.jenkinsTest.App.calculate());
+    	
+        //assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithfalse()
+    {
+    	assertEquals("Hello World1",testingPpt.jenkinsTest.App.calculate());
+    	
+        //assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAnswerWithfalse2()
+    {
+    	assertEquals("Hello World2",testingPpt.jenkinsTest.App.calculate());
+    	
+        //assertTrue( true );
+    }
+}
